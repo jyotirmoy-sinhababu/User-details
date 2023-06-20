886c81e882c0f8fc7136e0cc6e9c1317e9ab82b0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-21 00:25:27 +0530

    style added to mainPage

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import './mainPageStyle.css';
+
 import workingPersonImg from '../../assests/images/illustration-working.svg';
 
 const MainPage = () => {
   return (
     <div className='mainPage-cnt'>
       <div className='mainPage-txt-area'>
-        <h1>Make a list of users</h1>
-        <p>
-          Get your user details in sync. Keep every user details up to date with
-          our EDIT feature.
-        </p>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+          {' '}
+          <h1 className='mainPage-header'>Make a list of users</h1>
+          <p className='mainPage-para'>
+            Get your user details in sync. Keep every user details up to date
+            with our EDIT feature.
+          </p>
+        </div>
+
         <div>
           <button>Show my user</button>
         </div>
