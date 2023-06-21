8360188a56d472e0f77724ca0c75cd1d3d988b4d
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-21 16:36:18 +0530

    style added to addOn btn

--- src/pages/userList/UserLIst.jsx
+++ src/pages/userList/UserLIst.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './userListStyle.css';
 
 import Nav from '../../components/nav/Nav';
+import AddBtn from '../../components/addBtn/AddBtn';
 
 const UserLIst = () => {
   return (
@@ -10,6 +11,9 @@ const UserLIst = () => {
       <Nav />
       <div className='userList-noData-msg-cnt'>
         <h1 className='userList-noData-msg'>You have no users</h1>
+      </div>
+      <div>
+        <AddBtn />
       </div>
     </div>
   );
